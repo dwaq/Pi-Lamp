92db6645131018846d37ab511f249c133ec279d2
Dillon <Dillon@SaraAndrsonsMBP.home>
2017-01-20 20:10:42 -0500

    add lots of button logic from Spark-Lamp

--- send.cpp
+++ send.cpp
@@ -5,10 +5,17 @@
 
 #include "RCSwitch.h"
 #include "clickButton.h"
+#include <wiringPi.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 // "addresses" for the relays
+#define DILLON_ON   0x451533
+#define DILLON_OFF  0x45153C
+
+#define SARA_ON     0x4515C3
+#define SARA_OFF    0x4515CC
+
 #define LIQUOR_ON   0x451703
 #define LIQUOR_OFF  0x45170C
 
@@ -17,6 +24,14 @@
 
 // length of packet to send
 #define BIT_LENGTH 24
+
+// the lamp's state is stored in a two bit binary number
+const int DILLON_BIT  = 0b01; // bit 0 is the state of Dillon's lamp
+const int SARA_BIT  = 0b10;   // bit 1 is the state of Sara's lamp
+int lampState = 0b00;   // stores the state of both lamps
+
+void toggleDillon(RCSwitch mySwitch);
+void toggleSara(RCSwitch mySwitch);
 
 int main(int argc, char *argv[]) {
 
@@ -39,6 +54,66 @@ int main(int argc, char *argv[]) {
     ClickButton dillonButton(dillonLamp, LOW, CLICKBTN_PULLUP);
     ClickButton saraButton(saraLamp, LOW, CLICKBTN_PULLUP);
 
+    // set up the 2 switch pins as inputs with pullups
+    pinMode(dillonLamp, INPUT);
+    pullUpDnControl(dillonLamp, PUD_UP);
+    pinMode(saraLamp, INPUT);
+    pullUpDnControl(saraLamp, PUD_UP);
+
+    while(1) {
+        // Update buttons state
+        dillonButton.Update();
+        saraButton.Update();
+
+        // Dillon's button was clicked
+        if (dillonButton.clicks != 0){
+
+            if(dillonButton.clicks == 1){
+                //Serial.println("SINGLE click");
+                //blockInterrupt = //blockInterruptTimes;
+                toggleDillon(mySwitch);
+            }
+
+            if(dillonButton.clicks == 2){
+                //Serial.println("DOUBLE click");
+                //blockInterrupt = //blockInterruptTimes;
+                toggleSara(mySwitch);
+            }
+
+            if(dillonButton.clicks == -1){
+                //Serial.println("SINGLE LONG click");
+                //blockInterrupt = //blockInterruptTimes;
+                // TODO
+                //matchToggle("DILLON");
+            }
+        }
+
+        // Sara's button was clicked
+        if (saraButton.clicks != 0){
+
+            if(saraButton.clicks == 1){
+                //Serial.println("SINGLE click");
+                //blockInterrupt = //blockInterruptTimes;
+                toggleSara(mySwitch);
+            }
+
+            if(saraButton.clicks == 2){
+                //Serial.println("DOUBLE click");
+                //blockInterrupt = //blockInterruptTimes;
+                toggleDillon(mySwitch);
+            }
+
+            if(saraButton.clicks == -1){
+                //Serial.println("SINGLE LONG click");
+                //blockInterrupt = //blockInterruptTimes;
+                // TODO
+                // matchToggle("SARA");
+            }
+        }
+
+        delay(5);
+    }
+
     switch(command) {
         case 1:
             // turn liquor lights on
@@ -53,4 +128,36 @@ int main(int argc, char *argv[]) {
             return -1;
     }
 	return 0;
+}
+
+/* toggle Dillon's lamp */
+void toggleDillon(RCSwitch mySwitch){
+    // toggle the state of Dillon's lamp
+    lampState ^= DILLON_BIT;
+
+    // send new state
+    if ((lampState & DILLON_BIT) == DILLON_BIT){
+        //turn on Dillon's lamp
+        mySwitch.send(DILLON_ON, BIT_LENGTH);
+    }
+    else{
+        // turn off Dillon's lamp
+        mySwitch.send(DILLON_OFF, BIT_LENGTH);
+    }
+}
+
+/* toggle Sara's lamp */
+void toggleSara(RCSwitch mySwitch){
+    // toggle the state of Sara's lamp
+    lampState ^= SARA_BIT;
+
+    // send new state
+    if ((lampState & SARA_BIT) == SARA_BIT){
+        // turn on Sara's lamp
+        mySwitch.send(SARA_ON, BIT_LENGTH);
+    }
+    else{
+        // turn off Sara's lamp
+        mySwitch.send(SARA_OFF, BIT_LENGTH);
+    }
 }
