d9932edfd4f228bcbcff1fe3087a74103a0a5680
Dillon <ee.tinkerer@gmail.com>
2017-02-07 19:17:54 +0000

    incorperate client code into scan

--- Daemon/scan.py
+++ Daemon/scan.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import socket
 import sys, time
 from daemon import Daemon
 from bluepy.btle import Scanner, DefaultDelegate
@@ -17,10 +18,31 @@ class ScanDelegate(DefaultDelegate):
         if (dev.getValueText(AD_TYPE_UUID) == SWITCHMATE_UUID):
             data = dev.getValueText(AD_TYPE_SERVICE_DATA)
             # the bit at 0x0100 signifies if the switch is off or on
-            print ("off", "on")[(int(data, 16) >> 8) & 1]
+            status = ((int(data, 16) >> 8) & 1)
+
+            # Create a UDS socket
+            sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+            # Connect the socket to the port where the server is listening
+            server_address = '/tmp/pi-lamp-status'
+            print >>sys.stderr, 'connecting to %s' % server_address
+            try:
+                    sock.connect(server_address)
+            except socket.error, msg:
+                    print >>sys.stderr, msg
+                    sys.exit(1)
+
+            try:
+                # Send data
+                print >>sys.stderr, 'sending "%s"' % status
+                sock.sendall(status)
+            finally:
+                print >>sys.stderr, 'closing socket'
+                sock.close()
 
 class MyDaemon(Daemon):
     def run(self):
+        # scan for Switchmates
         scanner = Scanner().withDelegate(ScanDelegate())
         scanner.scan(0)
 
