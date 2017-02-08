bf86134d343f3443004c835b9fbc2917184a7e01
Dillon <ee.tinkerer@gmail.com>
2017-02-08 18:38:22 -0500

    add a way to kill thread from main

--- Threading/2threads.cpp
+++ Threading/2threads.cpp
@@ -3,21 +3,32 @@
 
 #include <chrono>
 
+int kill = 0;
+int *killPtr = &kill;
+
 void callFromThread(){
 	while(1) {
 		//delay(500);
 		std::this_thread::sleep_for(std::chrono::milliseconds(500));
-		std::cout << "Hello, thread" << std::endl;
+		std::cout << "Hello, thread. kill: " << *killPtr << std::endl;
+		if (*killPtr == 1) {
+			pthread_exit(NULL);
+		}
 	}
 }
 
 int main() {
 	std::thread t1(callFromThread);
 
-	while(1) {
+	for (int i = 0; i < 5; ++i) {
 		//delay(666);
 		std::this_thread::sleep_for(std::chrono::milliseconds(666));
-		std::cout << "Hello, main" << std::endl;
+
+		if (i == 2){
+			*killPtr = 1;
+		}
+
+		std::cout << "Hello, main. i: " << i << std::endl;
 	}
 
 	t1.join();
