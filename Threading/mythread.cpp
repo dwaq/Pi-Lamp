b91c4d4bdf73fbefafc4f844405f18497d803e9f
Dillon <ee.tinkerer@gmail.com>
2017-02-10 16:39:42 -0500

    change code to show that you can call functions from main or thread

--- Threading/mythread.cpp
+++ Threading/mythread.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <thread>
 
+void pr(void){
+std::cout << "function called" << std::endl;
+}
+
 void callFromThread(){
 	std::cout << "Hello, World" << std::endl;
+	pr();
 }
 
 int main() {
+	pr();
+
 	std::thread t1(callFromThread);
 
 	t1.join();
