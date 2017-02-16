b24931aad73cd6ca9fd21a1c112cb9335b9b6b10
Dillon <ee.tinkerer@gmail.com>
2017-02-15 19:12:56 -0500

    remove threadRAII

--- status.h
+++ status.h
@@ -46,24 +46,4 @@ int getSwitchState(void);
 
 void scan_service(void);
 
-// http://thispointer.com/c11-multithreading-part-2-joining-and-detaching-threads/
-class ThreadRAII
-{
-    std::thread & m_thread;
-    public:
-        ThreadRAII(std::thread  & thread) : m_thread(thread)
-        {
-
-        }
-
-        ~ThreadRAII()
-        {
-            // Check if thread is joinable then detach the thread
-            if(m_thread.joinable())
-            {
-                m_thread.detach();
-            }
-        }
-};
-
 #endif
