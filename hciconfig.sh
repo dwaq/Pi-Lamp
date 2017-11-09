55985f2a75a1b7d7439fa53217897e4a098cb337
Dillon <ee.tinkerer@gmail.com>
2017-11-09 18:42:48 +0000

    don't wait 5 seconds

--- hciconfig.sh
+++ hciconfig.sh
@@ -3,8 +3,7 @@
 echo "Attaching hci0..."
 until /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -
 do
-    echo "Bluetooth initialization failed, retrying in 5 seconds..."
-    sleep 5
+    echo "Bluetooth initialization failed, retrying..."
 done
 
 echo "Bringing hci0 up..."
