1d4ba9764bed30c419cbf6d8e5b25bac751dad8d
Dillon <ee.tinkerer@gmail.com>
2017-12-14 09:29:05 -0500

    log the data

--- switchmate/switchmate.cpp
+++ switchmate/switchmate.cpp
@@ -153,6 +153,9 @@ void process_data(uint8_t *data, size_t data_len)
     {
     	if (data[0] == SERVICE_DATA)
     	{
+        // log the full data message
+        logSwitchmateData(data);
+
         // last bit of the 7th data piece is the Switchmate status
         setSwitchState(int(data[7]) & 1);
     	}
