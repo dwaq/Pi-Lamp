ee55bd6e75523b7c740ed238c25e041f39df925e
Dillon <ee.tinkerer@gmail.com>
2018-01-15 15:18:35 -0500

    just use IP address

--- hue/hue.cpp
+++ hue/hue.cpp
@@ -19,10 +19,10 @@ int lampStatus(LampOwners owner){
     if(curl) {
         // set options based on owner
         if (owner == dillon){
-            curl_easy_setopt(curl, CURLOPT_URL, "philips-hue/api/29ocf3mMaJ1XAtbqeKM60A4dFen9tSc96u1JuQAi/lights/4");
+            curl_easy_setopt(curl, CURLOPT_URL, "192.168.1.16/api/29ocf3mMaJ1XAtbqeKM60A4dFen9tSc96u1JuQAi/lights/4");
         }
         else if (owner == sara){
-            curl_easy_setopt(curl, CURLOPT_URL, "philips-hue/api/29ocf3mMaJ1XAtbqeKM60A4dFen9tSc96u1JuQAi/lights/3");
+            curl_easy_setopt(curl, CURLOPT_URL, "192.168.1.16/api/29ocf3mMaJ1XAtbqeKM60A4dFen9tSc96u1JuQAi/lights/3");
         }
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
@@ -73,15 +73,15 @@ void switchLamp(LampOwners owner, bool on){
         // set options based on owner
         if (owner == dillon){
             std::cout << "Switching Dillon's lamp: ";
-            curl_easy_setopt(curl, CURLOPT_URL, "philips-hue/api/29ocf3mMaJ1XAtbqeKM60A4dFen9tSc96u1JuQAi/lights/4/state");
+            curl_easy_setopt(curl, CURLOPT_URL, "192.168.1.16/api/29ocf3mMaJ1XAtbqeKM60A4dFen9tSc96u1JuQAi/lights/4/state");
         }
         else if (owner == sara){
             std::cout << "Switching Sara's lamp: ";
-            curl_easy_setopt(curl, CURLOPT_URL, "philips-hue/api/29ocf3mMaJ1XAtbqeKM60A4dFen9tSc96u1JuQAi/lights/3/state");
+            curl_easy_setopt(curl, CURLOPT_URL, "192.168.1.16/api/29ocf3mMaJ1XAtbqeKM60A4dFen9tSc96u1JuQAi/lights/3/state");
         }
         else if (owner == both){
             std::cout << "Switching both lamps: ";
-            curl_easy_setopt(curl, CURLOPT_URL, "philips-hue/api/29ocf3mMaJ1XAtbqeKM60A4dFen9tSc96u1JuQAi/groups/1/action");
+            curl_easy_setopt(curl, CURLOPT_URL, "192.168.1.16/api/29ocf3mMaJ1XAtbqeKM60A4dFen9tSc96u1JuQAi/groups/1/action");
         }
         else{
             return;
